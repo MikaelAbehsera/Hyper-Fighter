e2fb7c84a8739109a664d35a01dd69d76938b4ea
Mikael Abehsera <mikael.abehsera@gmail.com>
2019-01-19 16:13:51 -0500

    trees are gone, added code for turret shoot

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -8,12 +8,10 @@ public class Turret : MonoBehaviour {
     public GameObject cannonball = null;
 
     public float minDelay = 1.0f;
-    public float delayTime = 0.0f;
+    public float maxDeplay = 0.0f;
 
     private float lastTime = 0.0f;
     private float delayTime = 0.0f;
-
-
 
 	// Use this for initialization
 	void Start () {
@@ -23,10 +21,28 @@ public class Turret : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         FollowPlayer();
+        Shoot();
 	}
 
     void FollowPlayer()
     {
         this.transform.LookAt(player);
+    }
+
+    void Shoot()
+    {
+        if(Time.time > lastTime + delayTime)
+        {
+            lastTime = Time.time;
+
+            delayTime = GetRandomValue();
+
+            GameObject obj = Instantiate(cannonball, this.transform.position, this.transform.rotation) as GameObject;
+        }
+    }
+
+    float GetRandomValue ()
+    {
+        return Random.Range(minDelay, maxDeplay);
     }
 }
