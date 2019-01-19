a05920dd6bf9681c35d735d809a1653a71362e2c
Mikael Abehsera <mikael.abehsera@gmail.com>
2019-01-19 11:31:20 -0500

    added move speeds

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Hero : MonoBehaviour {
 
     public float moveSpeed = 5.0f;
+    public float rotateSpeed = 5.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -18,8 +19,10 @@ public class Hero : MonoBehaviour {
 
     void Move()
     {
-        float move = Input.GetAxis("Vertical");
+        float move = Input.GetAxis("Vertical") * moveSpeed;
+        float rotation = Input.GetAxis("Horizontal") * rotateSpeed;
 
         this.transform.Translate(0, 0, move * Time.deltaTime);
+        this.transform.Translate(0, rotation * Time.deltaTime, 0);
     }
 }
