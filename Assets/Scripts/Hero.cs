f9c62ef4a00af65ab64125caafece13f0dd79173
Mikael Abehsera <mikael.abehsera@gmail.com>
2019-01-19 11:27:23 -0500

    added audio source to cannon ball and moving func to hero

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour {
 
+    public float moveSpeed = 5.0f;
+
 	// Use this for initialization
 	void Start () {
-		
+        Move();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void Move()
+    {
+        float move = Input.GetAxis("Vertical");
+
+        this.transform.Translate(0, 0, move * Time.deltaTime);
+    }
 }
